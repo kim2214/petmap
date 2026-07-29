9ae487e26e3b99f8b8fdc5da9ec26c771051ae79
kim2214 <anzel21@naver.com>
2026-07-29 22:04:16 +0900

    배포 버전 수정(1.5+6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.kimdev.petmap"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
